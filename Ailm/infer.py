01ce143265634c96d8be77a0e976b1bc1286b36a
Ian Kronquist <i_kronquist@apple.com>
2025-12-26 22:46:23 -0800

    Many bug fixes and features for inference

--- Ailm/infer.py
+++ Ailm/infer.py
@@ -1,3 +1,4 @@
+import random
 import queue
 import threading
 import time
@@ -20,35 +21,43 @@ def sample_topk(logits: mx.array, temperature: float=0.5, k: int = 5) -> mx.arra
     selected = args[batch_idx, random]
     return selected
 
+@mx.compile
+def sample_greedy(logits: mx.array, temperature: float=0.5, k: int = 5) -> mx.array:
+    token_ids = logits.argmax(axis=-1)
+    return token_ids
+
+# Translate names for strategies to our MLX implementations.
+SAMPLE_STRATEGIES = {
+        'topk': sample_topk,
+        'greedy': sample_greedy,
+        }
+
 def decode_and_print_worker(tokenizer: tiktoken.Encoding, work_queue: queue.Queue):
-    print("Starting decoder thread")
+    '''A worker thread so we can decode tokens and print them off of the main thread while training is happening.'''
     while True:
         try:
             batched_tokens = work_queue.get()
-            d0 = time.time()
-            print('got batch')
             tokens_list  = batched_tokens.tolist()
             tokens_list = typing.cast(list[list[int]], tokens_list)
             decoded = tokenizer.decode_batch(tokens_list)
-            d1 = time.time()
-            print('\n----\n'.join(decoded), '\ndecoding duration', d1-d0)
+            print('\n----\n'.join(decoded))
         except KeyError as e:
             print(e)
             decoded = ''
         except queue.ShutDown:
-            print('shutting down decoder thread')
             break
 
 class InferenceManager:
     '''In the spirit of removing as much work as possible from the training thread, this inference manager only encodes the prompt once, and performs decoding and printing on a separate thread.'''
-    def __init__(self, model: AilmV1, tokenizer: tiktoken.Encoding, max_tokens: int, max_batches: int, prompt: str, k: int, temperature: float):
-        self.model = model
+    def __init__(self, model: AilmV1, tokenizer: tiktoken.Encoding, max_tokens: int, max_batches: int, prompt: str, k: int, temperature: float, sample_strategy: str='topk', enable_kv_cache: bool = True):
+        self.model = mx.compile(model)
         self.tokenizer = tokenizer
         self.max_tokens = max_tokens
         self.max_batches = max_batches
         self.k = k
         self.prompt = prompt
         self.temperature = temperature
+        self.enable_kv_cache = enable_kv_cache
 
         tokenized_prompt = mx.array(tokenizer.encode(self.prompt))
 
@@ -58,32 +67,19 @@ class InferenceManager:
         self.batched_tokenized_prompt = mx.zeros((max_batches, self.prompt_tokens), dtype=mx.uint16)
         self.batched_tokens[:, 0:self.prompt_tokens] = tokenized_prompt
         self.batched_tokenized_prompt[:, 0:self.prompt_tokens] = tokenized_prompt
-        # def do_infer(batched_prompt: mx.array, i: int):
-        #     #print('do infer', i, batched_prompt[0, 0:(i+prompt_tokens)])
-        #     batch_view = batched_prompt[:, 0:(i+self.prompt_tokens)]
-        #     logits = model(batch_view)
-        #     logits = logits[:,-1]
-        #     chosen = sample_topk(logits, self.temperature, self.k)
-        #     #batched_prompt[:, i+prompt_tokens] = chosen
-        #     #return batched_prompt
-        #     return mx.concatenate([batch_view, chosen[:, None]], axis=-1)
-        
+
+        sample_function = SAMPLE_STRATEGIES[sample_strategy]
+
         def do_infer(batch: mx.array):
 
             logits = model(batch)
             logits = logits[:, -1]
-            chosen = sample_topk(logits, temperature, k)
-            new_batch = mx.concatenate(
-                [batch, chosen[:, None]],
-                axis=1
-            )
-            return new_batch
+            chosen = sample_function(logits, temperature, k)
+            return chosen
 
-        #self.do_infer = mx.compile(do_infer, inputs=[self.model.state, self.batched_tokens])
-        #self.do_infer = mx.compile(do_infer, inputs=[self.model.state])
         self.do_infer = do_infer
 
-        self.decoder_queue = queue.Queue(1)
+        self.decoder_queue: queue.Queue[mx.array] = queue.Queue(1)
         self.worker = threading.Thread(name='inference decoder', target=decode_and_print_worker, args=(tokenizer, self.decoder_queue,))
         self.worker.start()
 
@@ -91,20 +87,30 @@ class InferenceManager:
         '''Shutdown the work queue if we are ever deleted. This will cause the worker thread to finish.'''
         self.decoder_queue.shutdown()
         self.worker.join()
+
     def infer(self):
-        i0 = time.time()
         self.model.train(False)
-        self.model.reset_key_value_cache(True)
-        batch = self.batched_tokenized_prompt
-        i05 = time.time()
-        for _ in range(self.max_tokens):
-            batch = self.do_infer(batch)
-        i1 = time.time()
+        self.model.reset_key_value_cache(self.enable_kv_cache)
+        
+        # Start with the full prompt
+        todos = self.batched_tokenized_prompt
+        
+        for step in range(self.max_tokens):
+            if self.enable_kv_cache:
+                if step == 0:
+                    batch = todos
+                else:
+                    batch = todos[:, -1:]
+            else:
+                batch = todos
+
+            chosen = self.do_infer(batch)
+
+            # Append to our running sequence
+            todos = mx.concatenate([todos, chosen[:, None]], axis=-1)
         self.model.reset_key_value_cache(False)
         self.model.train(True)
-        self.decoder_queue.put(batch)
-        i2 = time.time()
-        print('>>>>>>>>>inference duration', i1-i05, i2-i0)
+        self.decoder_queue.put(todos)
 
 
 # MARK: Inference   
@@ -149,36 +155,58 @@ def infer(model: AilmV1, tokenizer: tiktoken.Encoding, max_tokens: int, max_batc
 
     return decoded
 
+# Setting type=bool in argparse.add_argument doesn't work the way you expect. This function borrowed from trusty old-fashioned stack overflow, can be passed to the type kwarg to create a boolean and get behavior similar to what you would expect if type was int or str.
+# https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 if __name__ == '__main__':
     import argparse
     supported_models = ['AilmV1']
 
     # Define the command line argument parser and parse any arguments provided.
     argparser = argparse.ArgumentParser(description='Run inference with the model')
-    argparser.add_argument('model', type=str, help='Safetensors or npz file with the model weights')
+    argparser.add_argument('models', type=str, nargs='+', help='Safetensors or npz file with the model weights')
     argparser.add_argument('--model-arch', type=str, help='The architecture of the model', choices=supported_models)
     argparser.add_argument('--token-count', type=int, default=50, help='The number of tokens to generate. Does not include the prompt.')
     argparser.add_argument('--batch-count', type=int, default=4, help='The number of batches of tokens to generate in parallel.')
     argparser.add_argument('--tokenizer', type=str, default='gpt2', help='The tokenizer to use.')
     argparser.add_argument('--temperature', type=float, default=0.5, help='The temperature for sampling.')
     argparser.add_argument('--top-k', type=int, default=4, help='The k cutoff value for sampling.')
+    argparser.add_argument('--sample-strategy', type=str, default='topk', choices=SAMPLE_STRATEGIES.keys(), help='Sample strategy.')
+    argparser.add_argument('--enable-kv-cache', type=str2bool, default=True, help='Enable the Key Value cache.')
     argparser.add_argument('--prompt', type=str, default='To be or not to be,', help='The prompt with which to start generation.')
+    argparser.add_argument('--enable-attention-sinks', type=bool, default=True, help='Forcibly enable or disable attention sinks.')
+    argparser.add_argument('--rng-seed', type=int, help='A seed for the random number generators.')
     args = argparser.parse_args()
+    print(f'Inference prompt is: "{args.prompt}"')
 
-    # Initialize the model and tokenizer.
-    config = AilmV1Config(no_rms_norm_weight=False)
-    model = AilmV1(config)
-    model.load_weights(args.model)
+    if args.rng_seed is not None:
+        random.seed(args.rng_seed)
+        mx.random.seed(args.rng_seed)
 
-    tokenizer = tiktoken.get_encoding(args.tokenizer)
+    for model_name in args.models:
+        print('model_name', model_name)
+        try:
+            # Initialize the model and tokenizer.
+            config = AilmV1Config(no_rms_norm_weight=False)
+            model = AilmV1(config)
+            model.load_weights(model_name )
 
-    # Perform inference and display the result.
-    # texts = infer(model, tokenizer, args.token_count, args.batch_count, args.prompt, args.top_k, args.temperature)
-    # text = '\n'.join(texts)
-    # print(text)
+            tokenizer = tiktoken.get_encoding(args.tokenizer)
 
-    inference_manager = InferenceManager(model, tokenizer, args.token_count, args.batch_count, args.prompt, args.top_k, args.temperature)
-    try:
-        inference_manager.infer()
-    finally:
-        inference_manager.finish()
+            inference_manager = InferenceManager(model, tokenizer, args.token_count, args.batch_count, args.prompt, args.top_k, args.temperature, sample_strategy=args.sample_strategy, enable_kv_cache=args.enable_kv_cache)
+            try:
+                inference_manager.infer()
+            finally:
+                inference_manager.finish()
+        except Exception as e:
+            print(e)
+            continue
